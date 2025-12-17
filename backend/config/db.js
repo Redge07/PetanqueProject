12ed039ebb1487e298b31bb0f740d587527f6147
Redge07 <148580943+Redge07@users.noreply.github.com>
2025-12-17 10:24:25 +0000

    incroyable

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,17 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: "srv2057.hstgr.io",
+  user: "u976121835_Redge7Petanque",
+  password: "Suarezbarca.9",
+  database: "u976121835_petanque",
+  port: 3306,
+
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  // port: process.env.DB_PORT,
 });
 
 module.exports = connection;
