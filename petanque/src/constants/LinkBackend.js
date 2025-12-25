db237bbecd48bcd26885111046b7ac9829178948
Redge07 <148580943+Redge07@users.noreply.github.com>
2025-12-25 17:23:37 +0000

    ip

--- petanque/src/constants/LinkBackend.js
+++ petanque/src/constants/LinkBackend.js
@@ -2,3 +2,5 @@ export const linkBackend = "http://localhost:5000/";
 // export const linkBackend = "https://petanqueproject.onrender.com/";
 
 //172.166.151.115
+//172.166.156.161
+//172.166.156.162
