2ca9432b5017872229175468dc90bb34e4f8ed71
Redge07 <mathonregis28@gmail.com>
2026-01-20 18:23:37 +0100

    winner cascade

--- backend/controllers2/winner.js
+++ backend/controllers2/winner.js
@@ -28,6 +28,7 @@ const placePlayerMatches2 = async (
   numero,
   pseudo,
   idTournament,
+  tour,
   groupe,
 ) => {
   const minNumberNoPlayerA = Math.min(
@@ -43,7 +44,7 @@ const placePlayerMatches2 = async (
   const lettre = minNumberNoPlayerA != Infinity ? "A" : "B";
   const matchNumber = lettre == "A" ? minNumberNoPlayerA : minNumberNoPlayerB;
   await query(
-    `update matches2 set id_player${lettre} = ?, pseudo_${lettre} = ? where id_tournament = ? and number = ? and class = 0 and groupe = ?`,
+    `update matches2 set id_player${lettre} = ?, pseudo_${lettre} = ? where id_tournament = ? and number = ? and class ${tour} and groupe = ?`,
     [numero, pseudo, idTournament, matchNumber, groupe],
   );
 };
@@ -64,22 +65,15 @@ exports.winnerArbre = async (req, res) => {
       "select * from matches2 where id_tournament = ? and class = ? and id_playerB = 0",
       [idTournament, tour / 2],
     );
-    const minNumberNoPlayerA = Math.min(
-      ...matches
-        .filter((match) => match.id_playerA == 0)
-        .map((match) => match.number),
+    await placePlayerMatches2(
+      matches,
+      win,
+      pseudoWin,
+      idTournament,
+      ">= 1",
+      "",
     );
-    const minNumberNoPlayerB = Math.min(
-      ...matches
-        .filter((match) => match.id_playerB == 0)
-        .map((match) => match.number),
-    );
-    const lettre = minNumberNoPlayerA != Infinity ? "A" : "B";
-    const matchNumber = lettre == "A" ? minNumberNoPlayerA : minNumberNoPlayerB;
-    await query(
-      `update matches2 set id_player${lettre} = ?, pseudo_${lettre} = ? where id_tournament = ? and number = ?`,
-      [win, pseudoWin, idTournament, matchNumber],
-    );
+
     await updatePlayers([win, lose], idTournament);
     return res.status(200).send("Victoire validé");
   } catch (err) {
